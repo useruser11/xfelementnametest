87e51b2d51caef72dc324ec5560ecc6c0c8de5fd
Dmitry <Dmitry@DESKTOP-DLSD42C>
2017-09-24 23:32:01 +0300

    Add project files.

---
+++ ElementNameTest/ElementNameTest/Page2ViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElementNameTest
+{
+    public class Page2ViewModel
+    {
+        public bool IsLayoutVisible
+        {
+            get
+            {
+                return true;
+            }
+            
+        }
+
+        public Page2ViewModel()
+        {
+
+        }
+    }
+}
---
+++ ElementNameTest/ElementNameTest/SimpleTemplateSelector.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ElementNameTest
+{
+    public class SimpleTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate IsTemplateOne { get; set; }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            return IsTemplateOne;
+        }
+    }
+}
